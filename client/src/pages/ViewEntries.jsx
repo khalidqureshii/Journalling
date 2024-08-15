0983729898cc13d378062553af89655f9de6eb0e
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-08-15 23:53:23 +0530

    Finally Added Back Buttons with Tailwind CSS

--- client/src/pages/ViewEntries.jsx
+++ client/src/pages/ViewEntries.jsx
@@ -2,6 +2,7 @@ import react, {useState, useEffect} from "react";
 import EntryCard from "../components/EntryCard";
 import useAuth from "../store/Auth";
 import FullDailyEntry from "../components/FullDailyEntry";
+import { useNavigate } from "react-router-dom";
 
 // Hi This is a Comment
 function ViewEntries(){
@@ -12,6 +13,7 @@ function ViewEntries(){
     const [isClicked, setClickEntry] = useState(false);
     const [currEntry, setCurrEntry] = useState([]);
 
+    const navigate = useNavigate();
 
     function createCards(entry){
         return <EntryCard data={entry} date={entry.date} updateClick={setClickEntry} updateEntry={setCurrEntry}/>
@@ -34,8 +36,9 @@ function ViewEntries(){
     
     }, [userID]);
     
+    const backButton = <><br /><button onClick={()=>navigate("/")}>Back</button><br /></>;
 
-    return (<> {(isClicked)? <FullDailyEntry data={currEntry}/> : (entries.length==0)?<h1>You Have Not Made Any Entries</h1>:entries.map(createCards) }</>);
+    return (<> {(isClicked)? <FullDailyEntry data={currEntry}/> : (entries.length==0)?<h1>You Have Not Made Any Entries</h1>:entries.map(createCards) }{backButton}</>);
 }
 
 export default ViewEntries;
