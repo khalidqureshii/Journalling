a866399c72ef554df8adad434b7435595c0699da
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-08-27 15:17:57 +0530

    changed Loading animation

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,8 +1,10 @@
 import react from "react";
 import PulseLoader from "react-spinners/PulseLoader";
+import RotateLoader from "react-spinners/RotateLoader";
 
 function Loader() {
-    return <PulseLoader color="#ffffff" margin={9} size={20} />
+    //return <PulseLoader color="#ffffff" margin={9} size={20} />
+    return <RotateLoader color="#ffffff" margin={12} size={25} />
 }
 
 export default Loader;
