a12b1afe7b9c581f98b9036088d25d9e5e74801b
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-08-28 21:52:06 +0530

    Clicking Brand Name takes to home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import react from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Header () {
     const navigate = useNavigate();
     return <header className="flex flex-row justify-between bg-[#2d2d2d] py-4">
-        <h1 className="text-3xl ml-7">MindFul Moments</h1>
+        <button onClick={()=>{navigate("/")}}><h1 className="text-3xl ml-7">MindFul Moments</h1></button>
 
         <button onClick={()=>{navigate("/logout")}}><h1 className="mr-7 text-xl">Logout</h1></button>
     </header>
