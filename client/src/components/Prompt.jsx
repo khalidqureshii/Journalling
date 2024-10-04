757bf82fcf5edd91d31c18c1ab85f95714f2845f
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-10-04 23:15:16 +0530

    Fixed Whitespace error

--- client/src/components/Prompt.jsx
+++ client/src/components/Prompt.jsx
@@ -4,7 +4,7 @@ function Prompt(props) {
     return (
         <div className="rounded-xl bg-[#2d2d2d] px-6 py-4 mx-7 my-7 max-w-5xl w-full">
             <h2 className="text-left text-2xl md:text-3xl mb-2">{props.question}</h2>
-            <h3 className="text-left text-xl mt-1">{props.answer}</h3>
+            <h3 className="text-left text-xl mt-1" style={{whiteSpace: "pre-wrap"}}>{props.answer}</h3>
         </div>
     );
 }
